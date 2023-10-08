94d1c97244682a083165529ef23e18f35942be22
yizhe <liaoyizhe75@gmail.com>
2023-10-08 18:18:34 +0800

    create new files

---
+++ CPE/UVa10055_Hashmat_the_brave_warrior.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long num1, num2;
+    while (scanf("%lld %lld", &num1, &num2) != EOF)
+    {
+        printf("%lld\n", abs(num1 - num2));
+    }
+
+    return 0;
+}
