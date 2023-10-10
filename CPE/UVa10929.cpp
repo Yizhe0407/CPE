ee4a5677a96dff2fc10481493063d5209000d481
yizhe <liaoyizhe75@gmail.com>
2023-10-10 11:00:05 +0800

    fix

--- CPE/UVa10929.cpp
+++ CPE/UVa10929.cpp
@@ -4,13 +4,19 @@
 int main()
 {
     long long figure;
-    scanf("%lld", figure);
-    if (figure % 11 == 0)
+    while (scanf("%lld", &figure))
     {
-        printf("%lld is a multiple of 11.");
-    }
-    else
-    {
-        printf("%lld is not a multiple of 11.");
+        if (figure == 0)
+        {
+            break;
+        }
+        if (figure % 11 == 0)
+        {
+            printf("%lld is a multiple of 11.\n", figure);
+        }
+        else
+        {
+            printf("%lld is not a multiple of 11.\n", figure);
+        }
     }
 }
