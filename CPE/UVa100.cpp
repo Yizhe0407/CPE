ed40ebf734d9ef606be6a77cc9159ac76a2b7eaa
yizhe <liaoyizhe75@gmail.com>
2023-10-10 09:52:59 +0800

    increase content

--- CPE/UVa100.cpp
+++ CPE/UVa100.cpp
@@ -4,12 +4,17 @@
 int main()
 {
     int n1, n2;
-    int count = 1, nextcount;
+    int count = 0, nextcount = 0;
     scanf("%d %d", &n1, &n2);
     for (int i = n1; i <= n2; i++)
     {
-        while (i != 1)
+        while (true)
         {
+            if (i == 1)
+            {
+                count += 1;
+                break;
+            }
             if (i % 2 != 0)
             {
                 i = 3 * i + 1;
