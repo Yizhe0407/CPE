d2f9c3a7e77659bc3c71bec5dfed7e59013a3fc7
yizhe <liaoyizhe75@gmail.com>
2023-10-15 10:31:09 +0800

    fix file

--- CPE/UVa10929.cpp
+++ CPE/UVa10929.cpp
@@ -1,22 +1,37 @@
 // You can say 11
+
 #include <stdio.h>
 
 int main()
 {
-    long long figure;
-    while (scanf("%lld", &figure))
-    {
-        if (figure == 0)
-        {
-            break;
-        }
-        if (figure % 11 == 0)
-        {
-            printf("%lld is a multiple of 11.\n", figure);
-        }
-        else
-        {
-            printf("%lld is not a multiple of 11.\n", figure);
-        }
-    }
+    char d[1000]; // 使用字符数组
+
+    printf("Enter a character: ");
+    scanf(" %c", &d[0]); // 请注意使用 %c 来读取字符，并且加一个空格来跳过可能的空白字符
+
+    printf("The character you entered is: %c\n", d[0]);
+
+    return 0;
 }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     long long figure;
+//     while (scanf("%lld", &figure))
+//     {
+//         if (figure == 0)
+//         {
+//             break;
+//         }
+//         if (figure % 11 == 0)
+//         {
+//             printf("%lld is a multiple of 11.\n", figure);
+//         }
+//         else
+//         {
+//             printf("%lld is not a multiple of 11.\n", figure);
+//         }
+//     }
+// }
