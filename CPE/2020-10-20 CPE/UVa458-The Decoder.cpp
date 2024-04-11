98072db7c4b73f6ea5fd7e9d8cb4ad01922d3f41
yizhe <liaoyizhe75@gmail.com>
2024-04-11 21:14:10 +0800

    add file

--- CPE/2020-10-20 CPE/UVa458-The Decoder.cpp
+++ CPE/2020-10-20 CPE/UVa458-The Decoder.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main()
 {
 	string s;
-	while(getline(cin,s)){
-		for(int i = 0;i < s.size();i++)
+	while (getline(cin, s))
+	{
+		for (int i = 0; i < s.size(); i++)
 			s[i] -= 7;
-		for(int i = 0;i < s.size();i++)
-			cout<<s[i];
-		cout<<endl;
+		for (int i = 0; i < s.size(); i++)
+			cout << s[i];
+		cout << endl;
 	}
 }
